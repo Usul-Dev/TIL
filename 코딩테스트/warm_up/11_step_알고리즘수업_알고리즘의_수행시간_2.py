b845679565f106e55a8ca33d6ed50603b55c7522
Usul <Usul-Dev@users.noreply.github.com>
2025-09-18 11:24:35 +0900

    chore: 2025-09-17 Coding Test

---
+++ 코딩테스트/warm_up/11_step_알고리즘수업_알고리즘의_수행시간_2.py
@@ -0,0 +1,12 @@
+"""
+https://www.acmicpc.net/problem/24263
+"""
+import sys
+
+input_data = int(sys.stdin.readline().strip())
+count = 0
+for _ in range(input_data):
+    count += 1
+
+print(count)
+print(1)
---
+++ 코딩테스트/warm_up/11_step_알고리즘수업_알고리즘의_수행시간_3.py
@@ -0,0 +1,8 @@
+"""
+https://www.acmicpc.net/problem/24264
+"""
+
+import sys
+input_data = int(sys.stdin.readline().strip())
+print(input_data*input_data)
+print(2)
